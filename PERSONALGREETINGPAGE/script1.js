cec4a57c8bd2911b177e97f5764eb29d248b5902
ujwalageekuru11-dot <ujwalageekuru11@gmail.com>
2026-06-20 20:55:48 +0530

    Add files via upload

---
+++ PERSONALGREETINGPAGE/script1.js
@@ -0,0 +1,7 @@
+let greeting = document.getElementById("greeting");
+let intro = document.getElementById("intro");
+
+greeting.textContent = "Hi, I'm Ujwala!";
+
+intro.style.color = "purple";
+intro.style.fontSize = "18px";
